ba853fd2c7709f9852da293841986473ec0ac393
RibamarErraGol <168042503+zeni101@users.noreply.github.com>
2025-09-20 18:10:12 -0300

    ListAvailableGardensController.ts

--- src/http/controllers/ListAvailableGardensController.ts
+++ src/http/controllers/ListAvailableGardensController.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
-import { createRentalUseCase } from "../UseCase/ListAvailableGardensUseCase";
+import { createRentalUseCase } from "../useCase/createRentalUseCase";
+
 
 export const createRentalController = async (req: Request, res: Response) => {
   try {
     const { customerId, carId, startDate, endDate, price } = req.body;
+
+
 
     if (!customerId || !carId || !startDate || !endDate || !price) {
       return res.status(400).json({ error: "Todos os campos são obrigatórios!" });
