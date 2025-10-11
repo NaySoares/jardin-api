11f40c96ab5591ef1c449f1e04efbf1fad75b83e
NaySoares <elienay.soares07@gmail.com>
2025-10-11 07:46:14 -0300

    feat: config cors

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,17 @@ import express, { NextFunction, Request, Response } from 'express'
 
 import { router } from './http/routes'
 
-// createConnection();
 const app = express()
+
+// Cors
+app.use((request: Request, response: Response, next: NextFunction) => {
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
+  next()
+})
 
 app.use(express.json())
 app.use(router)
